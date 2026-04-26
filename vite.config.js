2a07668b1393b6a56aed653ca0a9c5b5e0f0d36e
laofeng <1902768564@qq.com>
2026-04-26 18:11:34 +0800

    fix: resolve Markdown build error

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -15,4 +14,22 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          // 关键部分：将 .md 文件转为字符串，避免被当成 JS 解析
+          name: 'md-as-text',
+          transform(code, id) {
+            if (id.endsWith('.md')) {
+              return {
+                code: `export default ${JSON.stringify(code)}`,
+                map: null
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
 })
